2465243dea2e9ed96d6f58834e4ca82c5554bc26
Mohamed Fawzzi <mohamedfawzzimohamed@gmail.com>
2024-12-29 16:03:41 +0200

    x

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <div className='App'>
+    <div className='App overflow-hidden'>
       <Navbar/>
         <Outlet/>
       <Footer/>
